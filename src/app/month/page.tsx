71593980f1d1039526b69d671c7e09a7f9be30a1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-23 09:05:58 +0100

    show a month

---
+++ src/app/month/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { api } from "~/trpc/react";
+import { toDate, toSek } from "../utils/formatData";
+import MonthForm from "./components/MonthForm";
+import { useState } from "react";
+import { type tDatesSchema } from "~/zodSchemas";
+
+const Month = () => {
+  const [dates, setDates] = useState<tDatesSchema>({
+    from: new Date("2023-01-01"),
+    to: new Date("2023-01-31"),
+  });
+  console.log(dates);
+  return (
+    <section className="h-full">
+      <MonthForm onSubmit={(d) => setDates(d)} />
+      <Results dates={dates} />
+    </section>
+  );
+};
+
+type ResultsProps = { dates: { from: Date; to: Date } };
+const Results = ({ dates }: ResultsProps) => {
+  const { data, isSuccess, isLoading } = api.txs.getTxByDates.useQuery(dates);
+  return (
+    <ul className="h-full overflow-y-auto">
+      {isSuccess &&
+        data.map(({ datum, belopp, id }) => (
+          <li className="grid grid-cols-2" key={id}>
+            <p>{toDate(datum)}</p>
+            <p className={`text-right font-mono ${belopp < 0 && "text-red"}`}>
+              {toSek(belopp)}
+            </p>
+          </li>
+        ))}
+      {isLoading && <p>Laddar...</p>}
+    </ul>
+  );
+};
+
+export default Month;
