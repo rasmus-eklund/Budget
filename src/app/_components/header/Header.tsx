7a055adad11d4fc35f2e737ecbea385cc32aaf29
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-23 05:36:26 +0100

    Style

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -2,7 +2,10 @@ import Login from "./Login";
 
 const Header = () => {
   return (
-    <header className="flex justify-end items-center h-14 border-b bg-white">
+    <header className="relative flex h-14 w-full max-w-5xl items-center justify-end border-b bg-white px-3">
+      <h1 className="absolute left-[calc(50%-64px)] w-32 text-center text-xl font-bold text-red">
+        RICA Banken
+      </h1>
       <Login />
     </header>
   );
