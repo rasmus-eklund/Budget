b4dcca594f2ba064df26d6cb473d7755f550f2f9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-06 10:20:47 +0100

    add link to home

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -1,10 +1,11 @@
+import Link from "next/link";
 import Login from "./Login";
 
 const Header = () => {
   return (
     <header className="relative flex h-14 w-full max-w-5xl items-center justify-end border-b bg-white px-3">
       <h1 className="absolute left-[calc(50%-64px)] w-32 text-center text-xl font-bold text-red">
-        RICA Banken
+        <Link href={"/"}>RICA Banken</Link>
       </h1>
       <Login />
     </header>
