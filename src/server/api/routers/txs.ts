3d8743b29d84a0f279521781a4c770ce99c8f1d4
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-06 10:26:08 +0100

    implement mark internal and categorize transactions

--- src/server/api/routers/txs.ts
+++ src/server/api/routers/txs.ts
@@ -1,20 +1,48 @@
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import categorize from "~/utils/categorize";
+import { markInternal } from "~/utils/findInternal";
+import { decimalToNumber } from "~/utils/formatData";
 import { datesSchema } from "~/zodSchemas";
 
 export const txsRouter = createTRPCRouter({
   getTxByDates: protectedProcedure
     .input(datesSchema)
     .query(async ({ ctx, input: { from, to } }) => {
+      const userId = ctx.session.user.id;
+      const response = await ctx.db.budgetgrupp.findMany({
+        where: { userId },
+      });
+      const categories = response.map(({ namn, matches }) => ({
+        namn,
+        matches,
+      }));
       const data = await ctx.db.txs.findMany({
-        where: { AND: [{ datum: { gte: from } }, { datum: { lte: to } }] },
+        where: {
+          AND: [
+            { konto: { Person: { userId } } },
+            { datum: { gte: from } },
+            { datum: { lte: to } },
+          ],
+        },
         include: {
           konto: { select: { namn: true, Person: { select: { namn: true } } } },
         },
       });
-      return data.map(({ belopp, saldo, ...rest }) => ({
+      const kontoPerson = data.map(({ konto, kontoId: _, ...rest }) => ({
         ...rest,
-        belopp: Number(belopp),
-        saldo: Number(saldo),
+        konto: konto.namn,
+        person: konto.Person.namn,
       }));
+      const toNumber = decimalToNumber(kontoPerson);
+      const internal = markInternal(toNumber);
+      return internal.map((tx) => {
+        if (tx.budgetgrupp === "Övrigt") {
+          return {
+            ...tx,
+            budgetgrupp: categorize(tx.text, categories),
+          };
+        }
+        return tx;
+      });
     }),
 });
