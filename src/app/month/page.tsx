7416e34a637d3b60cea8f710324e2664bcf954a1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-07 11:16:39 +0100

    split out components

--- src/app/month/page.tsx
+++ src/app/month/page.tsx
@@ -1,44 +1,55 @@
-"use client";
-import { api } from "~/trpc/react";
+import { api } from "~/trpc/server";
 import DateFilter from "./components/DateFilter";
 import Tabs from "../_components/Tabs";
 import Transaction from "./components/Transaction";
+import Aggregated from "./components/Aggregated";
+import { Suspense } from "react";
+import parseSearch from "~/utils/parseUrlDates";
 
-const Month = () => {
+type Props = {
+  searchParams: Record<string, string | string[] | undefined>;
+};
+const Month = ({ searchParams }: Props) => {
+  const dates = parseSearch({ searchParams });
   return (
     <section className="h-full">
-      <DateFilter>
-        {({ filter }) => (
-          <Tabs
-            tabs={[
-              { name: "Budget", tab: <Budget dates={filter} /> },
-              { name: "Transaktioner", tab: <Results dates={filter} /> },
-            ]}
-          />
-        )}
-      </DateFilter>
+      <DateFilter />
+      <Tabs
+        tabs={[
+          { name: "Budget", tab: <Budget dates={dates} /> },
+          { name: "Transaktioner", tab: <Results dates={dates} /> },
+        ]}
+      />
     </section>
   );
 };
 
-type Props = { dates: { from: Date; to: Date } };
-const Results = ({ dates }: Props) => {
-  const { data, isSuccess, isLoading } = api.txs.getTxByDates.useQuery(dates);
+type Dates = { dates: { from: Date; to: Date } };
+const Results = async ({ dates }: Dates) => {
+  const data = await api.txs.getTxByDates.query(dates);
   return (
-    <ul className="flex h-[calc(100%-40px)] flex-col gap-1 overflow-y-auto">
-      {isSuccess && data.map((d) => <Transaction key={d.id} data={d} />)}
-      {isLoading && <p>Laddar...</p>}
-    </ul>
+    <Suspense fallback={<p>Laddar...</p>}>
+      <ul className="flex h-[calc(100%-40px)] flex-col gap-1 overflow-y-auto">
+        {data.map((d) => (
+          <Transaction key={d.id} data={d} />
+        ))}
+      </ul>
+    </Suspense>
   );
 };
 
-const Budget = ({ dates }: Props) => {
-  const { data, isSuccess, isLoading } = api.txs.getTxByDates.useQuery(dates);
+const Budget = async ({ dates }: Dates) => {
+  const [data, categories] = await Promise.all([
+    api.txs.getTxByDates.query(dates),
+    api.txs.getCategories.query(),
+  ]);
+
   return (
-    <ul className="flex h-[calc(100%-40px)] flex-col gap-1 overflow-y-auto">
-      {isSuccess && <p>budget</p>}
-      {isLoading && <p>Laddar...</p>}
-    </ul>
+    <div className="flex h-[calc(100%-40px)] flex-col gap-1 overflow-y-auto">
+      <Suspense fallback={<p>Laddar...</p>}>
+        <Aggregated data={data} categories={categories} />
+      </Suspense>
+    </div>
   );
 };
 
