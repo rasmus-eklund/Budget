6867cd94dba7f1bb4d23735a933dbbd2467b7efb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-10 09:06:54 +0100

    measure time

---
+++ src/utils/calculateTimeDelta.ts
@@ -0,0 +1,16 @@
+type Dates = { start: Date; end: Date };
+const timeDelta = ({ start, end }: Dates): string => {
+  const diff = end.getTime() - start.getTime();
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  const formatted = [
+    hours.toString().padStart(2, "0"),
+    minutes.toString().padStart(2, "0"),
+    seconds.toString().padStart(2, "0"),
+  ].join(":");
+
+  return `Time delta: ${formatted}`;
+};
+
+export default timeDelta;
