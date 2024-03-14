e0aacb7a10fb22b198435734319b732ee8239d5b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-14 09:39:21 +0100

    add internal and typ

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,5 +1,21 @@
+const types = [
+  "Insättning",
+  "Korttransaktion",
+  "Övrigt",
+  "Uttag",
+  "Autogiro",
+  "Pg-bg",
+  "E-faktura",
+  "Pg-Bg",
+  "Utlandsbetalning",
+] as const;
+
+export type Typ = (typeof types)[number];
+
 export type TxFilter = {
   category: string;
   person: string;
   inom: boolean;
 };
+
+export type Internal = { id: string; belopp: number; typ: Typ; konto: string };
