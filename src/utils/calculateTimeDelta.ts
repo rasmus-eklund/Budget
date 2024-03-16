2aed6902a136e5fea6244dc54114d7982353bfb1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-16 08:30:37 +0100

    return seconds and ms

--- src/utils/calculateTimeDelta.ts
+++ src/utils/calculateTimeDelta.ts
@@ -1,16 +1,9 @@
 type Dates = { start: Date; end: Date };
 const timeDelta = ({ start, end }: Dates): string => {
   const diff = end.getTime() - start.getTime();
-  const hours = Math.floor(diff / (1000 * 60 * 60));
-  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-  const formatted = [
-    hours.toString().padStart(2, "0"),
-    minutes.toString().padStart(2, "0"),
-    seconds.toString().padStart(2, "0"),
-  ].join(":");
-
-  return `Time delta: ${formatted}`;
+  const milliseconds = diff % 1000;
+  return `${seconds} s, ${milliseconds} ms`;
 };
 
 export default timeDelta;
