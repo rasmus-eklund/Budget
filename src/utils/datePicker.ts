7dbbc449c29450c9d0261fa3627568ba438203d4
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-16 07:45:05 +0100

    add datepicker helpers and tests

---
+++ src/utils/datePicker.ts
@@ -0,0 +1,50 @@
+import { type FromTo } from "~/zodSchemas";
+
+export const minusOneMonth = (p: FromTo) => {
+  const from = new Date(p.from);
+  const to = new Date(p.to);
+  from.setMonth(from.getMonth() - 1);
+  to.setMonth(to.getMonth() - 1);
+  return { from, to };
+};
+export const minusOneDay = (p: FromTo) => {
+  const from = new Date(p.from);
+  const to = new Date(p.to);
+  from.setDate(from.getDate() - 1);
+  to.setDate(to.getDate() - 1);
+  return { from, to };
+};
+export const plusOneDay = (p: FromTo) => {
+  const from = new Date(p.from);
+  const to = new Date(p.to);
+  from.setDate(from.getDate() + 1);
+  to.setDate(to.getDate() + 1);
+  return { from, to };
+};
+export const plusOneMonth = (p: FromTo) => {
+  const from = new Date(p.from);
+  const to = new Date(p.to);
+  from.setMonth(from.getMonth() + 1);
+  to.setMonth(to.getMonth() + 1);
+  return { from, to };
+};
+export const incrementMonth = (
+  year: number,
+  month: number,
+): [number, number] => {
+  if (month < 11) {
+    return [year, month + 1];
+  } else {
+    return [year + 1, 0];
+  }
+};
+export const decrementMonth = (
+  year: number,
+  month: number,
+): [number, number] => {
+  if (month > 0) {
+    return [year, month - 1];
+  } else {
+    return [year - 1, 11];
+  }
+};
