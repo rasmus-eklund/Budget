43db2b45b318b9b627e7974dc7ad37cfa190cd28
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-16 07:46:05 +0100

    rename variable

--- src/utils/parseUrlDates.ts
+++ src/utils/parseUrlDates.ts
@@ -1,9 +1,9 @@
-import { datesSchema, type tDatesSchema } from "~/zodSchemas";
+import { datesSchema, type FromTo } from "~/zodSchemas";
 
 type Props = {
   searchParams: Record<string, string | string[] | undefined>;
 };
-const parseSearch = ({ searchParams }: Props): tDatesSchema => {
+const parseSearch = ({ searchParams }: Props): FromTo => {
   const parsed = datesSchema.safeParse(searchParams);
   if (!parsed.success) {
     return { from: new Date(), to: new Date() };
