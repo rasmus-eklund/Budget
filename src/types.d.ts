53ff75f12b5c5552739309bf6d077fed3ec1e816
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-18 09:36:01 +0100

    add person to Internal

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,13 @@
-
 export type TxFilter = {
   category: string;
   person: string;
   inom: boolean;
 };
 
-export type Internal = { id: string; belopp: number; typ: Typ; konto: string };
+export type Internal = {
+  id: string;
+  belopp: number;
+  typ: Typ;
+  konto: string;
+  person: string;
+};
