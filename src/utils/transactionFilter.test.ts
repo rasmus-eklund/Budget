6741ea83ac0842fa45acb7900133c6b93b3d2c2b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-19 14:57:28 +0100

    add account to filter

--- src/utils/transactionFilter.test.ts
+++ src/utils/transactionFilter.test.ts
@@ -3,22 +3,32 @@ import transactionFilter from "./transactionFilter";
 import { type TxFilter } from "~/types";
 
 const data = [
-  { person: "A", budgetgrupp: "mat" },
-  { person: "A", budgetgrupp: "transport" },
-  { person: "A", budgetgrupp: "inom" },
-  { person: "B", budgetgrupp: "mat" },
-  { person: "B", budgetgrupp: "transport" },
-  { person: "B", budgetgrupp: "inom" },
+  { person: "A", budgetgrupp: "mat", konto: "A" },
+  { person: "A", budgetgrupp: "transport", konto: "B" },
+  { person: "A", budgetgrupp: "inom", konto: "A" },
+  { person: "B", budgetgrupp: "mat", konto: "B" },
+  { person: "B", budgetgrupp: "transport", konto: "A" },
+  { person: "B", budgetgrupp: "inom", konto: "B" },
 ];
 
 describe("Transaction filter", () => {
   it("should show all", () => {
-    const filter: TxFilter = { category: "", inom: true, person: "" };
+    const filter: TxFilter = {
+      category: "",
+      inom: true,
+      person: "",
+      account: "",
+    };
     const result = data.filter((d) => transactionFilter({ ...d, filter }));
     expect(result).toEqual(data);
   });
   it("should show only person A", () => {
-    const filter: TxFilter = { category: "", inom: true, person: "A" };
+    const filter: TxFilter = {
+      category: "",
+      inom: true,
+      person: "A",
+      account: "",
+    };
     const expected = [
       { person: "A", budgetgrupp: "mat" },
       { person: "A", budgetgrupp: "transport" },
@@ -28,13 +38,23 @@ describe("Transaction filter", () => {
     expect(result).toEqual(expected);
   });
   it("should show only person A and mat category", () => {
-    const filter: TxFilter = { category: "mat", inom: true, person: "A" };
+    const filter: TxFilter = {
+      category: "mat",
+      inom: true,
+      person: "A",
+      account: "",
+    };
     const expected = [{ person: "A", budgetgrupp: "mat" }];
     const result = data.filter((d) => transactionFilter({ ...d, filter }));
     expect(result).toEqual(expected);
   });
   it("should hide inom", () => {
-    const filter: TxFilter = { category: "", inom: false, person: "" };
+    const filter: TxFilter = {
+      category: "",
+      inom: false,
+      person: "",
+      account: "",
+    };
     const expected = [
       { person: "A", budgetgrupp: "mat" },
       { person: "A", budgetgrupp: "transport" },
@@ -45,7 +65,12 @@ describe("Transaction filter", () => {
     expect(result).toEqual(expected);
   });
   it("should show only person A and hide inom", () => {
-    const filter: TxFilter = { category: "", inom: false, person: "A" };
+    const filter: TxFilter = {
+      category: "",
+      inom: false,
+      person: "A",
+      account: "",
+    };
     const expected = [
       { person: "A", budgetgrupp: "mat" },
       { person: "A", budgetgrupp: "transport" },
--- src/utils/transactionFilter.ts
+++ src/utils/transactionFilter.ts
@@ -1,8 +1,14 @@
 import { type TxFilter } from "~/types";
 
 const transactionFilter = <
-  T extends { person: string; budgetgrupp: string; filter: TxFilter },
+  T extends {
+    person: string;
+    budgetgrupp: string;
+    konto: string;
+    filter: TxFilter;
+  },
 >({
+  konto,
   person,
   budgetgrupp,
   filter,
@@ -10,8 +16,9 @@ const transactionFilter = <
   const personMatch = filter.person === "" || person === filter.person;
   const categoryMatch =
     filter.category === "" || budgetgrupp === filter.category;
+  const accountMatch = filter.account === "" || konto === filter.account;
   const inomMatch = filter.inom || budgetgrupp !== "inom";
-  return personMatch && categoryMatch && inomMatch;
+  return personMatch && categoryMatch && accountMatch && inomMatch;
 };
 
 export default transactionFilter;
