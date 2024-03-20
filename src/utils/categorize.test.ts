0c1b1dd983b48392a1609a7f86031f16389561d0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-20 09:53:14 +0100

    edit tests

--- src/utils/categorize.test.ts
+++ src/utils/categorize.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from "vitest";
 import categorize from "./categorize";
 
 describe("categorize", () => {
-  it("should categorize correctly", () => {
+  it("should categorize Ica as mat", () => {
     const cats = [
       { namn: "mat", matches: ["ica", "willys"] },
       { namn: "transport", matches: ["sl"] },
@@ -10,6 +10,16 @@ describe("categorize", () => {
     const result = categorize("Ica Nära Kragstalund", cats);
 
     expect(result).toStrictEqual("mat");
+  });
+  it("should categorize Lön as Lön", () => {
+    const cats = [
+      { namn: "lön", matches: ["lön"] },
+      { namn: "mat", matches: ["ica", "willys"] },
+      { namn: "transport", matches: ["sl"] },
+    ];
+    const result = categorize("Lön", cats);
+
+    expect(result).toStrictEqual("lön");
   });
   it("should handle no match", () => {
     const cats = [
