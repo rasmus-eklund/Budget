deb8d632ffea7749d31783e7fbfd1195550c7e73
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-20 09:52:33 +0100

    feat: show txs per year

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,26 +1,45 @@
+import { api } from "~/trpc/server";
 import SubmitButton from "../_components/SubmitButton";
 import { upload } from "./actions/uploadActions";
 
-const page = () => {
+const page = async () => {
+  const counts = await api.txs.getCountsPerYear.query();
+  counts.sort((a, b) => a.year - b.year);
   return (
-    <form className="flex flex-col gap-2 p-4" action={upload}>
-      <p>
-        Transaktionerna du laddar upp kommer att ersätta alla transaktioner från
-        året du väljer:
-      </p>
-      <div className="flex items-center gap-2">
-        <label htmlFor="year">År</label>
-        <input
-          type="number"
-          name="year"
-          id="year"
-          defaultValue={new Date().getFullYear()}
-        />
+    <div className="flex flex-col gap-4 p-4">
+      <div>
+        <h2>Din data:</h2>
+        <ul className="flex flex-col gap-2">
+          {counts.map(({ count, year }) => (
+            <li key={year}>
+              <p>
+                År {year} har {count} transaktioner.
+              </p>
+            </li>
+          ))}
+        </ul>
       </div>
-      <p>Filnamn måste vara i form av {"<Person>_<Konto>.csv"}</p>
-      <input type="file" name="files" multiple accept=".csv" />
-      <SubmitButton />
-    </form>
+      <form className="flex flex-col gap-2" action={upload}>
+        <p>
+          Transaktionerna du laddar upp kommer att ersätta alla transaktioner
+          från året du väljer:
+        </p>
+        <div className="flex items-center gap-2">
+          <label htmlFor="year">År</label>
+          <input
+            type="number"
+            name="year"
+            id="year"
+            defaultValue={new Date().getFullYear()}
+          />
+        </div>
+        <p>Filnamn måste vara i form av Person_Konto.csv</p>
+        <input type="file" name="files" multiple accept=".csv" />
+        <div>
+          <SubmitButton />
+        </div>
+      </form>
+    </div>
   );
 };
 
