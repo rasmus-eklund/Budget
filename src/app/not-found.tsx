607c8fdddfd9ed68a64870fb50ee90a85aaaeab8
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-29 07:48:11 +0100

    not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <p>Sidan finns inte</p>
+      <Link className="text-blue-500" href={"/month"}>
+        Gå tillbaks till dina bankloggar.
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
