62eee34aef451616671d53478a077e3d1582ebc0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-03-29 05:57:56 +0100

    remove unused props

--- src/app/month/page.tsx
+++ src/app/month/page.tsx
@@ -2,9 +2,6 @@ import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
 import PasswordLayer from "./_components/PasswordLayer";
 
-type Props = {
-  searchParams: Record<string, string | string[] | undefined>;
-};
 const Month = async () => {
   const session = await getServerAuthSession();
   if (!session) {
