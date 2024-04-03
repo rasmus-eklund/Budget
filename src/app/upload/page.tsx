c0f2dbb5160162120cdcf558c2cbe2539c87b244
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-03 16:33:54 +0200

    Feat: add suspense

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import FileForm from "./_components/FileForm";
 import { getTxsPerYear } from "./actions/uploadActions";
 
@@ -7,19 +8,24 @@ const page = async () => {
     <div className="flex flex-col gap-4 p-4">
       <div>
         <h2>Din data:</h2>
-        <ul className="flex flex-col gap-2">
-          {counts.map(({ count, year }) => (
-            <li key={year}>
-              <p>
-                År {year} har {count} transaktioner.
-              </p>
-            </li>
-          ))}
-        </ul>
+        <Suspense fallback={<Loading />}>
+          <ul className="flex flex-col gap-2">
+            {counts.map(({ count, year }) => (
+              <li key={year}>
+                <p>
+                  År {year} har {count} transaktioner.
+                </p>
+              </li>
+            ))}
+          </ul>
+        </Suspense>
       </div>
       <FileForm />
     </div>
   );
 };
 
+const Loading = () => {
+  return <p>Laddar...</p>;
+};
 export default page;
