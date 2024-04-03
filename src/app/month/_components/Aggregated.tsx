dc4179d9deb4b1bbeaa4673248a3a51d6fd13a9d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-03 16:55:10 +0200

    Style: Table

--- src/app/month/_components/Aggregated.tsx
+++ src/app/month/_components/Aggregated.tsx
@@ -20,42 +20,51 @@ const Aggregated = ({
       .filter((tx) => tx.person === person && tx.budgetgrupp === category)
       .reduce((a, c) => a + c.belopp, 0);
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className="text-left">Kategori</th>
-          {people.map((person) => (
-            <th className="text-right" key={uuid()}>
-              {person}
+    <div className="overflow-x-auto py-2">
+      <table className="min-w-full divide-y divide-gray-200">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider text-gray-500">
+              Kategori
             </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {categories.map((category) => {
-          const sums = people.map((person) => ({
-            person,
-            sum: getPersonCatSum(person, category),
-          }));
-          return (
-            <tr key={uuid()}>
-              <td>{capitalize(category)}</td>
-              {people.map((person) => {
-                const sum = sums.find((i) => i.person === person)!.sum;
-                return (
-                  <td
-                    className={`text-right ${sum < 0 ? "text-red-600" : ""}`}
-                    key={uuid()}
-                  >
-                    {toSek(sum)}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+            {people.map((person) => (
+              <th
+                className="px-6 py-3 text-right text-xs font-medium uppercase tracking-wider text-gray-500"
+                key={uuid()}
+              >
+                {person}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="divide-y divide-gray-200 bg-white">
+          {categories.map((category) => {
+            const sums = people.map((person) => ({
+              person,
+              sum: getPersonCatSum(person, category),
+            }));
+            return (
+              <tr key={uuid()}>
+                <td className="whitespace-nowrap p-1 font-semibold tracking-wider">
+                  {capitalize(category)}
+                </td>
+                {people.map((person) => {
+                  const sum = sums.find((i) => i.person === person)!.sum;
+                  return (
+                    <td
+                      className={`text-right ${sum < 0 ? "text-red-600" : ""}`}
+                      key={uuid()}
+                    >
+                      {toSek(sum)}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
