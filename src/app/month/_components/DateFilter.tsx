d81edd6dbed22fe45799dc9fbd4c9090aaf891b6
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-03 15:47:13 +0200

    Refactor: use shad tabs

--- src/app/month/_components/DateFilter.tsx
+++ src/app/month/_components/DateFilter.tsx
@@ -1,37 +1,33 @@
 "use client";
-import Tabs from "~/components/common/Tabs";
 import FreeDates from "./FreeDates";
 import YearMonth from "./YearMonth";
 import { Button } from "~/components/ui/button";
 import type { FromTo } from "~/lib/zodSchemas";
+import { TabsContent, TabsList, TabsTrigger, Tabs } from "~/components/ui/tabs";
 
 type Props = { changeDates: (dates: FromTo) => void };
 const DateFilter = ({ changeDates }: Props) => {
   return (
-    <Tabs
-      tabs={[
-        {
-          name: "År månad",
-          tab: (
-            <YearMonth changeDate={changeDates}>
-              <Button variant={"secondary"} type="submit">
-                Ok
-              </Button>
-            </YearMonth>
-          ),
-        },
-        {
-          name: "Fritt spann",
-          tab: (
-            <FreeDates changeDate={changeDates}>
-              <Button variant={"secondary"} type="submit">
-                Ok
-              </Button>
-            </FreeDates>
-          ),
-        },
-      ]}
-    />
+    <Tabs defaultValue="month">
+      <TabsList>
+        <TabsTrigger value="month">Månad</TabsTrigger>
+        <TabsTrigger value="free">Fritt spann</TabsTrigger>
+      </TabsList>
+      <TabsContent value="month">
+        <YearMonth changeDate={changeDates}>
+          <Button variant={"secondary"} type="submit">
+            Ok
+          </Button>
+        </YearMonth>
+      </TabsContent>
+      <TabsContent value="free">
+        <FreeDates changeDate={changeDates}>
+          <Button variant={"secondary"} type="submit">
+            Ok
+          </Button>
+        </FreeDates>
+      </TabsContent>
+    </Tabs>
   );
 };
 
