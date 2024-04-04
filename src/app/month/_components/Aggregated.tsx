9882fba41a501b9bb5c95e1e06163d8f4f92fc7f
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-04 16:15:51 +0200

    Refactor: Move logic out of render

--- src/app/month/_components/Aggregated.tsx
+++ src/app/month/_components/Aggregated.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { v4 as uuid } from "uuid";
 import capitalize from "~/lib/utils/capitalize";
 import { toSek } from "~/lib/utils/formatData";
@@ -9,16 +10,26 @@ type Props = { data: Tx[]; options: Uniques; loading: boolean };
 const Aggregated = ({
   loading,
   data,
-  options: { people, categories: cats },
+  options: { people, categories },
 }: Props) => {
+  const sumsMemo = useMemo(() => {
+    const sums: Record<string, Record<string, number>> = {};
+    for (const category of categories) {
+      sums[category] = {};
+      for (const person of people) {
+        sums[category]![person] = 0;
+      }
+    }
+    for (const { budgetgrupp, person, belopp } of data) {
+      sums[budgetgrupp]![person] += belopp;
+    }
+    return sums;
+  }, [data, people, categories]);
+
   if (loading) {
     return <p>Laddar...</p>;
   }
-  const categories = cats.filter((i) => i != "inom");
-  const getPersonCatSum = (person: string, category: string) =>
-    data
-      .filter((tx) => tx.person === person && tx.budgetgrupp === category)
-      .reduce((a, c) => a + c.belopp, 0);
+
   return (
     <div className="overflow-x-auto py-2">
       <table className="min-w-full divide-y divide-gray-200">
@@ -38,30 +49,23 @@ const Aggregated = ({
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200 bg-white">
-          {categories.map((category) => {
-            const sums = people.map((person) => ({
-              person,
-              sum: getPersonCatSum(person, category),
-            }));
-            return (
+          {categories
+            .filter((i) => i != "inom")
+            .map((category) => (
               <tr key={uuid()}>
                 <td className="whitespace-nowrap p-1 font-semibold tracking-wider">
                   {capitalize(category)}
                 </td>
-                {people.map((person) => {
-                  const sum = sums.find((i) => i.person === person)!.sum;
-                  return (
-                    <td
-                      className={`text-right ${sum < 0 ? "text-red-600" : ""}`}
-                      key={uuid()}
-                    >
-                      {toSek(sum)}
-                    </td>
-                  );
-                })}
+                {people.map((person) => (
+                  <td
+                    className={`text-right ${sumsMemo[category]![person]! < 0 ? "text-red-600" : ""}`}
+                    key={uuid()}
+                  >
+                    {toSek(sumsMemo[category]![person]!)}
+                  </td>
+                ))}
               </tr>
-            );
-          })}
+            ))}
         </tbody>
       </table>
     </div>
