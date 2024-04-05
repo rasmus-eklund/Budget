9572050b31aafbc30b0453525f18d4449431a457
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-05 10:10:37 +0200

    remove unused

--- src/lib/utils/calculateTimeDelta.ts
+++
@@ -1,9 +0,0 @@
-type Dates = { start: Date; end: Date };
-const timeDelta = ({ start, end }: Dates): string => {
-  const diff = end.getTime() - start.getTime();
-  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-  const milliseconds = diff % 1000;
-  return `${seconds} s, ${milliseconds} ms`;
-};
-
-export default timeDelta;
