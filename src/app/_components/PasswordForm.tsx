f7bb4c6c3213ce20fd2b3286dad78aa71169ffe0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-08 13:30:42 +0200

    Feat: password form

---
+++ src/app/_components/PasswordForm.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+
+import { type Passwords, passwordsSchema } from "~/lib/zodSchemas";
+
+type Props = {
+  onSubmit: (password: string) => void;
+};
+
+const PasswordForm = ({ onSubmit }: Props) => {
+  const form = useForm<Passwords>({
+    mode: "all",
+    resolver: zodResolver(passwordsSchema),
+    defaultValues: { password: "", confirm: "" },
+  });
+
+  const handleSubmit = async (data: Passwords) => {
+    onSubmit(data.password);
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Lösenord</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirm"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Repetera lösenord</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+              <FormDescription>
+                Lösenordet används för att kryptera och avkryptera dina
+                bankloggar.
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={!form.formState.isValid}>
+          Använd lösenordet
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default PasswordForm;
