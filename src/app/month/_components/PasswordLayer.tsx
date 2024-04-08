77f4a08efb04d650d6ed0b191a99b659b29f62bf
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-08 07:50:38 +0200

    style

--- src/app/month/_components/PasswordLayer.tsx
+++ src/app/month/_components/PasswordLayer.tsx
@@ -11,6 +11,9 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import TransactionFilter from "./TransactionFilter";
 import applyTransactionFilters from "~/lib/utils/transactionFilter";
 import type { TxFilter, TxReturn, TxSort } from "~/types";
+import { Label } from "~/components/ui/label";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 
 type Tab = "aggregated" | "transactions";
 
@@ -24,7 +27,13 @@ const PasswordLayer = () => {
     message: "Fel lösenord",
   });
   const defaults: { txFilter: TxFilter; txSort: TxSort } = {
-    txFilter: { category: "", person: "", account: "", inom: false },
+    txFilter: {
+      category: "",
+      person: "",
+      account: "",
+      inom: false,
+      search: "",
+    },
     txSort: { belopp: "Datum (Lågt-Högt)" },
   };
   const [txFilter, setTxFilter] = useState<TxFilter>(defaults.txFilter);
@@ -96,20 +105,21 @@ const PasswordForm = ({ submitPassword }: PasswordFormProps) => {
   const [pw, setPw] = useState("");
   return (
     <form
+      className="flex flex-col gap-2"
       onSubmit={(e) => {
         e.preventDefault();
         submitPassword(pw);
       }}
     >
-      <label htmlFor="pw">Lösenord</label>
-      <input
+      <Label htmlFor="pw">Lösenord</Label>
+      <Input
         type="password"
         name="password"
         id="pw"
         value={pw}
         onChange={({ target: { value } }) => setPw(value)}
       />
-      <button type="submit">Ok</button>
+      <Button type="submit">Ok</Button>
     </form>
   );
 };
