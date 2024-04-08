57d0d86c42f2fc6cf06794b375a88484df365724
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-08 14:19:18 +0200

    Refactor: move table to component

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,25 +1,19 @@
 import { Suspense } from "react";
 import FileForm from "./_components/FileForm";
 import { getTxsPerYear } from "./actions/uploadActions";
+import Hide from "./_components/Hide";
+import YearCountTable from "./_components/YearCountTable";
 
 const page = async () => {
-  const counts = await getTxsPerYear();
+  const data = await getTxsPerYear();
   return (
     <div className="flex flex-col gap-4 p-4">
-      <div>
+      <Hide>
         <h2>Din data:</h2>
         <Suspense fallback={<Loading />}>
-          <ul className="flex flex-col gap-2">
-            {counts.map(({ count, year }) => (
-              <li key={year}>
-                <p>
-                  År {year} har {count} transaktioner.
-                </p>
-              </li>
-            ))}
-          </ul>
+          <YearCountTable data={data} />
         </Suspense>
-      </div>
+      </Hide>
       <FileForm />
     </div>
   );
