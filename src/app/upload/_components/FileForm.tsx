3a5270979e39b97347c54174f11a064d4d236334
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-08 14:19:37 +0200

    Refactor: remove password form and move logic to other files

--- src/app/upload/_components/FileForm.tsx
+++ src/app/upload/_components/FileForm.tsx
@@ -1,65 +1,34 @@
 "use client";
 
-import { type FormEvent, useState } from "react";
+import { type FormEvent, useState, useEffect } from "react";
 import { ClipLoader } from "react-spinners";
 import Transactions from "~/app/month/_components/Transactions";
 import { Button } from "~/components/ui/button";
-import { markInternal } from "~/lib/utils/findInternal";
-import parseTxs from "~/lib/utils/parseTxs";
 import type { Tx } from "~/lib/zodSchemas";
-import SubmitForm from "./SubmitForm";
-import { getFileNames, hasCorrectFilenames } from "./fileFormHelpers";
+import {
+  getFileNames,
+  hasCorrectFilenames,
+  readFiles,
+  uploadFiles,
+} from "./fileFormHelpers";
 import toast from "react-hot-toast";
 import TransactionFilter from "~/app/month/_components/TransactionFilter";
 import applyTransactionFilters from "~/lib/utils/transactionFilter";
 import getUnique from "~/lib/utils/getUnique";
 import type { TxFilter, TxSort } from "~/types";
-
-const readFiles = async (
-  files: FileList,
-  updatePercent: (percent: number) => void,
-) => {
-  const data: Tx[] = [];
-  for (const file of files) {
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    const [person, konto_end] = file.name.split("_") as [string, string];
-    const konto = konto_end.replace(".csv", "").replace("Ã¤", "ä");
-    const txs = await parseTxs(buffer, person, konto);
-    data.push(...txs);
-  }
-  const internal = markInternal(data, updatePercent);
-  return internal;
-};
+import { usePassword } from "~/app/_components/PasswordContext";
+import { sortOptions } from "~/lib/utils";
 
 const FileForm = () => {
+  const { password, showDialog } = usePassword();
   const [files, setFiles] = useState<FileList | null>(null);
   const [txs, setTxs] = useState<Tx[]>([]);
-  const defaults: { txFilter: TxFilter; txSort: TxSort } = {
-    txFilter: {
-      category: "",
-      person: "",
-      account: "",
-      inom: false,
-      search: "",
-    },
-    txSort: { belopp: "Datum (Lågt-Högt)" },
-  };
-  const [txFilter, setTxFilter] = useState<TxFilter>(defaults.txFilter);
-  const [txSort, setTxSort] = useState<TxSort>(defaults.txSort);
   const [loading, setLoading] = useState({ loading: false, percent: 0 });
-  const options = getUnique(txs);
-  const applyFilters = () => {
-    const filtered = applyTransactionFilters({
-      data: txs,
-      filters: { txFilter, txSort },
-    });
-    return <Transactions data={filtered} loading={false} />;
-  };
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const [error, setError] = useState({ error: false, message: "" });
+
+  const processTxs = async () => {
     if (!files) {
-      throw new Error("no files");
+      return setError({ error: true, message: "Inga filer valda" });
     }
     setLoading({ loading: true, percent: 0 });
     const data = await readFiles(files, (percent) => {
@@ -70,11 +39,25 @@ const FileForm = () => {
     const y = new Set(data.map((i) => i.datum.getFullYear()));
     if (y.size !== 1) {
       setLoading({ loading: false, percent: 0 });
-      throw new Error("Ett år per uppladdning");
+      setError({ error: true, message: "Ett år per uppladdning" });
     }
     setTxs(data);
     setLoading({ loading: false, percent: 0 });
   };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading({ loading: true, percent: 0.78 });
+    await uploadFiles({ password, txs });
+    setFiles(null);
+    setTxs([]);
+    setLoading({ loading: false, percent: 0 });
+  };
+  useEffect(() => {
+    if (!password) {
+      showDialog({ open: true });
+    }
+  }, [password, showDialog]);
   return (
     <div>
       <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
@@ -110,36 +93,63 @@ const FileForm = () => {
             ))}
           </ul>
         )}
-        <div>
-          <p>Att bearbeta filer kan ta flera minuter.</p>
+        {error.error && <p className="text-lg text-red-500">{error.message}</p>}
+        <p>Att bearbeta filer kan ta flera minuter.</p>
+        <div className="flex gap-2">
           {loading.loading ? (
-            <Button disabled>
+            <Button type="button" disabled>
               <ClipLoader size={20} className="mr-2" />
               Vänta {(loading.percent * 100).toFixed(0)} %
             </Button>
           ) : (
-            <Button disabled={!files || files.length === 0}>Bearbeta</Button>
+            <Button
+              onClick={processTxs}
+              type="button"
+              disabled={!files || files.length === 0}
+            >
+              Bearbeta
+            </Button>
           )}
+          <Button disabled={!txs || txs.length === 0 || !password}>
+            Ladda upp
+          </Button>
         </div>
       </form>
-      {txs.length !== 0 ? (
-        <div>
-          <SubmitForm
-            txs={txs}
-            onSubmit={() => {
-              setFiles(null);
-              setTxs([]);
-            }}
-          />
-          <TransactionFilter
-            options={options}
-            defaults={defaults}
-            filters={{ txFilter, txSort }}
-            setFilters={{ setTxFilter, setTxSort }}
-          />
-          {applyFilters()}
-        </div>
-      ) : null}
+      {txs.length !== 0 ? <ShowTransactions txs={txs} /> : null}
+    </div>
+  );
+};
+
+const ShowTransactions = ({ txs }: { txs: Tx[] }) => {
+  const defaults: { txFilter: TxFilter; txSort: TxSort } = {
+    txFilter: {
+      category: "",
+      person: "",
+      account: "",
+      inom: false,
+      search: "",
+    },
+    txSort: { sort: sortOptions.dateAsc },
+  };
+  const options = getUnique(txs);
+  const [txFilter, setTxFilter] = useState<TxFilter>(defaults.txFilter);
+  const [txSort, setTxSort] = useState<TxSort>(defaults.txSort);
+  const applyFilters = () => {
+    const filtered = applyTransactionFilters({
+      data: txs,
+      filters: { txFilter, txSort },
+    });
+    return <Transactions data={filtered} loading={false} />;
+  };
+  return (
+    <div>
+      <TransactionFilter
+        options={options}
+        defaults={defaults}
+        filters={{ txFilter, txSort }}
+        setFilters={{ setTxFilter, setTxSort }}
+      />
+      {applyFilters()}
     </div>
   );
 };
