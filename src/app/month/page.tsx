041f0f7fb15d7b6607da5eb99e2a95d402ebb186
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-09 07:44:47 +0200

    Feat: get unique years

--- src/app/month/page.tsx
+++ src/app/month/page.tsx
@@ -1,13 +1,15 @@
 import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
 import FilterLayer from "./_components/FilterLayer";
+import { getYears } from "./dataLayer/getYears";
 
 const Month = async () => {
   const session = await getServerAuthSession();
   if (!session) {
     redirect("/");
   }
-  return <FilterLayer />;
+  const years = await getYears();
+  return <FilterLayer years={years} />;
 };
 
 export default Month;
