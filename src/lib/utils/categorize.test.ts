30e2639eacf7bce66cbf02c633ed31a3337dcf55
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-10 14:45:34 +0200

    Refactor: use type

--- src/lib/utils/categorize.test.ts
+++ src/lib/utils/categorize.test.ts
@@ -1,9 +1,10 @@
 import { describe, expect, it } from "vitest";
-import categorize from "./categorize";
+import { categorize } from "./categorize";
+import { type Category } from "~/types";
 
 describe("categorize", () => {
   it("should categorize Ica as mat", () => {
-    const cats = [
+    const cats: Category[] = [
       { name: "mat", match: [{ name: "ica" }, { name: "willys" }] },
       { name: "transport", match: [{ name: "sl" }] },
     ];
@@ -12,8 +13,11 @@ describe("categorize", () => {
     expect(result).toStrictEqual("mat");
   });
   it("should find item in middle of string", () => {
-    const cats = [
-      { name: "mat", match: [{ name: "ica" }, { name: "willys" }, { name: "matmat" }] },
+    const cats: Category[] = [
+      {
+        name: "mat",
+        match: [{ name: "ica" }, { name: "willys" }, { name: "matmat" }],
+      },
       { name: "transport", match: [{ name: "sl" }, { name: "sj" }] },
     ];
     const result = categorize("K*MatMat.Se", cats);
@@ -21,7 +25,7 @@ describe("categorize", () => {
     expect(result).toStrictEqual("mat");
   });
   it("should categorize correctly distinguish similar items", () => {
-    const cats = [
+    const cats: Category[] = [
       { name: "mat", match: [{ name: "ica" }, { name: "willys" }] },
       { name: "lån", match: [{ name: "abababb" }] },
       { name: "transport", match: [{ name: "sl" }, { name: "abababc" }] },
@@ -31,7 +35,7 @@ describe("categorize", () => {
     expect(result).toStrictEqual("lån");
   });
   it("should handle no match", () => {
-    const cats = [
+    const cats: Category[] = [
       { name: "mat", match: [{ name: "ica" }, { name: "willys" }] },
       { name: "transport", match: [{ name: "sl" }] },
     ];
