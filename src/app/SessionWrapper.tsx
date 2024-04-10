19c17523d580572415f20c9a46682f8c27fca703
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-10 16:53:01 +0200

    Lint

--- src/app/SessionWrapper.tsx
+++ src/app/SessionWrapper.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-
 type Props = {
   children: React.ReactNode;
 };
+
 const SessionWrapper = ({ children }: Props) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
