4fc6b31850c76b20f5f769dbb71ca05ab3c4eba2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-10 14:49:27 +0200

    Change directory name

---
+++ src/app/transactions/_components/TransactionFilter.tsx
@@ -0,0 +1,160 @@
+import type { TxSort, TxFilter, SortOption } from "~/types";
+import capitalize from "~/lib/utils/capitalize";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
+import { Button } from "~/components/ui/button";
+import { useState } from "react";
+import { sortOptions } from "~/lib/utils";
+
+type Filters = { txFilter: TxFilter; txSort: TxSort };
+type Props = {
+  options: { categories: string[]; people: string[]; accounts: string[] };
+  filters: Filters;
+  defaults: Filters;
+  setFilters: {
+    setTxFilter: (txFilter: TxFilter) => void;
+    setTxSort: (txSort: TxSort) => void;
+  };
+};
+const TransactionFilter = ({
+  options,
+  defaults,
+  filters: { txFilter, txSort },
+  setFilters: { setTxFilter, setTxSort },
+}: Props) => {
+  const [search, setSearch] = useState("");
+  return (
+    <form
+      className="flex flex-col gap-2 p-1 md:flex-row md:justify-between"
+      onSubmit={(e) => {
+        e.preventDefault();
+        setTxFilter({ ...txFilter, search });
+      }}
+    >
+      <div className="flex items-center gap-2">
+        <Select
+          value={txFilter.category}
+          onValueChange={(value) =>
+            setTxFilter({ ...txFilter, category: value })
+          }
+        >
+          <SelectTrigger className="w-[160px]">
+            <SelectValue placeholder="" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="none">Kategori</SelectItem>
+            <SelectSeparator />
+            {options.categories.map((i) => (
+              <SelectItem key={i} value={i}>
+                {capitalize(i)}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Select
+          value={txFilter.person}
+          onValueChange={(value) => setTxFilter({ ...txFilter, person: value })}
+        >
+          <SelectTrigger className="w-[160px]">
+            <SelectValue placeholder="" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="none">Person</SelectItem>
+            <SelectSeparator />
+            {options.people.map((i) => (
+              <SelectItem key={i} value={i}>
+                {capitalize(i)}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Select
+          value={txFilter.account}
+          onValueChange={(value) =>
+            setTxFilter({ ...txFilter, account: value })
+          }
+        >
+          <SelectTrigger className="w-[160px]">
+            <SelectValue placeholder="" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="none">Konto</SelectItem>
+            <SelectSeparator />
+            {options.accounts.map((i) => (
+              <SelectItem key={i} value={i}>
+                {capitalize(i)}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <Input
+          placeholder="Sök"
+          value={search}
+          onChange={({ target: { value } }) => setSearch(value)}
+        />
+      </div>
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex items-center gap-2">
+          <Label className="text-nowrap" htmlFor="inomCheck">
+            Visa inom
+          </Label>
+          <Input
+            className="size-5"
+            checked={txFilter.inom}
+            onChange={() => setTxFilter({ ...txFilter, inom: !txFilter.inom })}
+            id="inomCheck"
+            type="checkbox"
+          />
+          {JSON.stringify(txFilter) !== JSON.stringify(defaults.txFilter) && (
+            <Button
+              type="button"
+              onClick={() => setTxFilter(defaults.txFilter)}
+            >
+              Rensa filter
+            </Button>
+          )}
+        </div>
+        <Select
+          value={txSort.sort}
+          onValueChange={(value) =>
+            setTxSort({ ...txSort, sort: value as SortOption })
+          }
+        >
+          <SelectTrigger className="w-[160px]">
+            <SelectValue placeholder="" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>Datum</SelectLabel>
+              <SelectItem value={sortOptions.dateAsc}>Datumstigande</SelectItem>
+              <SelectItem value={sortOptions.dateDesc}>
+                Datumfallande
+              </SelectItem>
+            </SelectGroup>
+            <SelectGroup>
+              <SelectLabel>Belopp</SelectLabel>
+              <SelectItem value={sortOptions.amountAsc}>
+                Lågt till högt
+              </SelectItem>
+              <SelectItem value={sortOptions.amountDesc}>
+                Högt till lågt
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+    </form>
+  );
+};
+
+export default TransactionFilter;
---
+++ src/app/transactions/_components/Transactions.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { dateToString, toSek } from "~/lib/utils/formatData";
+import { Virtuoso } from "react-virtuoso";
+import type { Tx } from "~/lib/zodSchemas";
+import capitalize from "~/lib/utils/capitalize";
+
+type Props = { data: Tx[] };
+
+const Transactions = ({ data }: Props) => {
+  return (
+    <ul className="flex flex-col gap-2">
+      <Virtuoso
+        className="!h-[500px]"
+        data={data}
+        itemContent={(_, tx) => <Transaction key={tx.id} data={tx} />}
+      />
+    </ul>
+  );
+};
+
+type TransactionProps = { data: Tx };
+
+const Transaction = ({
+  data: { belopp, datum, budgetgrupp, person, konto, text },
+}: TransactionProps) => {
+  return (
+    <li className="mb-2 mt-2 flex flex-col rounded-sm bg-red-50 p-1 shadow-lg">
+      <div className="grid grid-cols-2">
+        <div>
+          <p className="font-semibold">{dateToString(datum)}</p>
+        </div>
+        <p className={`text-right font-mono ${belopp < 0 && "text-red-600"}`}>
+          {toSek(belopp)}
+        </p>
+      </div>
+      <div className="flex justify-between gap-2">
+        <p className="overflow-hidden text-ellipsis whitespace-nowrap">
+          {text} - <i>{capitalize(budgetgrupp)}</i>
+        </p>
+        <p className="whitespace-nowrap">
+          {person} ({konto})
+        </p>
+      </div>
+    </li>
+  );
+};
+
+export default Transactions;
