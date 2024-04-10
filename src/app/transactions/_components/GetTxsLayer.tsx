a75696b3e817e4ec16fde52c0ab55a4ea31ff203
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-10 16:54:32 +0200

    Refactor: client fetch

--- src/app/transactions/_components/GetTxsLayer.tsx
+++ src/app/transactions/_components/GetTxsLayer.tsx
@@ -1,23 +1,55 @@
 "use client";
-import React from "react";
-import { useTxs } from "~/lib/hooks/useTxs";
+import React, { useEffect, useState } from "react";
 import ShowData from "./ShowData";
 import { type FromTo } from "~/lib/zodSchemas";
+import { usePassword } from "~/app/_components/PasswordContext";
+import { getCurrentYearMonth } from "~/lib/utils/datePicker";
+import getTxByDates from "../dataLayer/getData";
+import { type TxReturn } from "~/types";
+import Link from "next/link";
 
 type Props = { range: FromTo };
 const GetTxsLayer = ({ range }: Props) => {
-  const { data, loading, setTxDate } = useTxs();
-  if (!data.success) {
-    if (data.message === "Fel lösenord") {
-      return <p>Fel lösenord</p>;
-    }
+  const { password } = usePassword();
+  const [txDate, setTxDate] = useState<FromTo>(getCurrentYearMonth());
+  const [loading, setLoading] = useState(false);
+  const [{ data, status }, setData] = useState<TxReturn>({
+    data: [],
+    status: "Success",
+  });
+
+  useEffect(() => {
+    setLoading(true);
+    getTxByDates({ dates: txDate, password })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      })
+      .catch(() => setData({ data: [], status: "Error" }));
+  }, [password, txDate]);
+
+  if (status === "Wrong password") {
+    return (
+      <p>
+        Fel lösenord.{" "}
+        <span>
+          <Link href={"/password"} className="underline">
+            Ändra till rätt lösenord
+          </Link>
+        </span>
+        .
+      </p>
+    );
+  }
+  if (status === "Error") {
     return <p>Något gick fel</p>;
   }
+
   return (
     <ShowData
       loading={loading}
-      data={data.data}
-      setDates={(dates) => setTxDate(dates)}
+      data={data}
+      setDates={async (dates) => setTxDate(dates)}
       range={range}
     />
   );
