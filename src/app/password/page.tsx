84630acf17707abd5c5905eb67e2cfc4910a27a8
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-10 17:44:48 +0200

    Refactor: only form

--- src/app/password/page.tsx
+++ src/app/password/page.tsx
@@ -1,17 +1,9 @@
 "use client";
 import React from "react";
-import PasswordForm from "../_components/PasswordForm";
-import { usePassword } from "../_components/PasswordContext";
-import { useRouter } from "next/navigation";
+import PasswordForm from "./_components/PasswordForm";
 
 const Password = () => {
-  const router = useRouter();
-  const { updatePassword } = usePassword();
-  const onSubmit = (password: string) => {
-    updatePassword(password);
-    router.push("/transactions");
-  };
-  return <PasswordForm onSubmit={onSubmit} />;
+  return <PasswordForm />;
 };
 
 export default Password;
