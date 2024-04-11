b999c0f6b2b64028bf3cb7f5bde55754cffd06ab
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-11 17:26:36 +0200

    Test: from to

--- src/lib/utils/getYearRange.test.ts
+++ src/lib/utils/getYearRange.test.ts
@@ -1,5 +1,5 @@
 import { it, describe, expect } from "vitest";
-import { getYearRange } from "./getYearRange";
+import { getFromTo, getYearRange } from "./getYearRange";
 
 describe("get year range", () => {
   it("should be 1 to 10", () => {
@@ -8,5 +8,19 @@ describe("get year range", () => {
     expect(range).toStrictEqual([
       2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
     ]);
+  });
+});
+
+describe("max min date", () => {
+  it("should find max and min", () => {
+    const data = [
+      { datum: new Date("2021-01-01") },
+      { datum: new Date("2022-02-01") },
+      { datum: new Date("2023-03-01") },
+      { datum: new Date("2024-04-01") },
+    ];
+    const { from, to } = getFromTo(data);
+    expect(from).toStrictEqual(data[0]?.datum);
+    expect(to).toStrictEqual(data[3]?.datum);
   });
 });
