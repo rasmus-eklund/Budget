fc69226f1de68e582b9fb660252d2d5be057a7d0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-12 15:46:02 +0200

    Refactor: use memo and change default range

--- src/app/demo/_components/DemoPage.tsx
+++ src/app/demo/_components/DemoPage.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { getFromTo } from "~/lib/utils/dateCalculations";
-
 import type { FromTo, Tx } from "~/lib/zodSchemas";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { applyCategories } from "~/lib/utils/categorize";
 import ShowData from "~/components/common/ShowData";
 import DateFilter from "~/components/common/DateFilters/DateFilter";
 import { categories } from "./_generateData/categories";
 import { generateData } from "./_generateData/generateData";
+import { getFromTo } from "~/lib/utils/dateCalculations";
 
 const DemoPage = () => {
-  const txs = generateData();
-  const fromTo = getFromTo(txs);
+  const txs = useMemo(() => generateData(), []);
+  const range = getFromTo(txs);
+  const fromTo = { from: new Date("2023-12-01"), to: new Date("2023-12-31") };
   const [{ from, to }, setDates] = useState<FromTo>(fromTo);
   const data: Tx[] = [];
   for (const tx of txs) {
@@ -24,7 +24,7 @@ const DemoPage = () => {
   return (
     <ShowData data={data}>
       <DateFilter
-        range={fromTo}
+        range={range}
         changeDates={async (dates) => setDates(dates)}
       />
     </ShowData>
