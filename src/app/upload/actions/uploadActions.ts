db40f5bbd3e7bbcf8fe55e56375a8affa2b86581
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-15 11:01:10 +0200

    Refactor: new schema

--- src/app/upload/actions/uploadActions.ts
+++ src/app/upload/actions/uploadActions.ts
@@ -2,23 +2,27 @@
 
 import { revalidatePath } from "next/cache";
 import { db } from "~/server/db";
-import { and, eq, sql } from "drizzle-orm";
-import { category, txs } from "~/server/db/schema";
+import { eq, sql } from "drizzle-orm";
+import {
+  type InsertTx,
+  category,
+  persons,
+  txs,
+  users,
+  bankAccounts,
+} from "~/server/db/schema";
 import getUserId from "~/server/getUserId";
-import type { DbTx } from "~/types";
 
 export const upload = async ({
   transactions,
   year,
 }: {
-  transactions: DbTx[];
+  transactions: InsertTx[];
   year: number;
 }) => {
-  const userId = await getUserId();
-  await db
-    .delete(txs)
-    .where(and(eq(txs.year, Number(year)), eq(txs.userId, userId)));
-  await db.insert(txs).values(transactions.map((i) => ({ ...i, userId })));
+  await getUserId();
+  await db.delete(txs).where(eq(txs.year, Number(year)));
+  await db.insert(txs).values(transactions);
   revalidatePath("/upload");
 };
 
@@ -29,8 +33,11 @@ export const getTxsPerYear = async () => {
       year: txs.year,
       count: sql<number>`cast(count(${txs.id}) as int)`,
     })
-    .from(txs)
-    .where(eq(txs.userId, userId))
+    .from(users)
+    .where(eq(users.id, userId))
+    .innerJoin(persons, eq(persons.userId, userId))
+    .innerJoin(bankAccounts, eq(bankAccounts.personId, persons.id))
+    .innerJoin(txs, eq(txs.bankAccountId, bankAccounts.id))
     .groupBy(txs.year)
     .orderBy(txs.year);
   return data;
@@ -44,4 +51,13 @@ export const GetCategories = async () => {
     with: { match: { columns: { name: true } } },
   });
   return categories;
+};
+
+export const getPersonAccounts = async () => {
+  const userId = await getUserId();
+  return await db.query.persons.findMany({
+    columns: { id: true, name: true },
+    where: eq(persons.userId, userId),
+    with: { bankAccounts: { columns: { name: true, id: true } } },
+  });
 };
