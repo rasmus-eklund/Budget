b67a257ef4e9ff30be8f5c7eee0553d33f11b8ed
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-15 11:00:20 +0200

    Refactor: new schema

--- src/app/transactions/dataLayer/getDateRange.ts
+++ src/app/transactions/dataLayer/getDateRange.ts
@@ -1,14 +1,16 @@
 import { eq, max, min } from "drizzle-orm";
 import { db } from "~/server/db";
-import { txs } from "~/server/db/schema";
+import { bankAccounts, persons, txs } from "~/server/db/schema";
 import getUserId from "~/server/getUserId";
 
 export const getDateRange = async () => {
   const userId = await getUserId();
   const range = await db
     .select({ from: min(txs.date), to: max(txs.date) })
-    .from(txs)
-    .where(eq(txs.userId, userId));
+    .from(persons)
+    .innerJoin(bankAccounts, eq(bankAccounts.personId, persons.id))
+    .innerJoin(txs, eq(txs.bankAccountId, bankAccounts.id))
+    .where(eq(persons.userId, userId));
   if (range[0]) {
     const { from, to } = range[0];
     if (!from || !to) {
