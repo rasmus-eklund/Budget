600d4aed9a1bf5565983e3018bdb666460c1d6e9
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-13 06:57:38 +0200

    add bar chart

---
+++ src/app/upload/_components/YearCountChart.tsx
@@ -0,0 +1,28 @@
+"use client";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+type Props = { data: { year: number; count: number }[] };
+
+const YearCountChart = ({ data }: Props) => {
+  return (
+    <ResponsiveContainer height={200} minWidth={"100%"}>
+      <BarChart data={data}>
+        <CartesianGrid />
+        <Tooltip cursor={{ fill: "hsl(var(--muted))" }} />
+        <XAxis dataKey={"year"} />
+        <YAxis dataKey={"count"} />
+        <Bar dataKey={"count"} name={"Antal"} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default YearCountChart;
