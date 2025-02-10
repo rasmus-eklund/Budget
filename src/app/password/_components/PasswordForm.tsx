49737506789c7c6c5dc2ba94edea16bf701dab79
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-10 06:52:30 +0100

    remove confirm

--- src/app/password/_components/PasswordForm.tsx
+++ src/app/password/_components/PasswordForm.tsx
@@ -21,20 +21,17 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 const defaultValues = {
   password: env.NEXT_PUBLIC_PASS ?? "",
-  confirm: env.NEXT_PUBLIC_PASS ?? "",
 };
 const PasswordForm = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const from = searchParams.get("from");
+  const error = searchParams.get("error");
   const { password, updatePassword } = usePassword();
   const form = useForm<Passwords>({
     mode: "all",
     resolver: zodResolver(passwordsSchema),
-    defaultValues:
-      defaultValues.password === ""
-        ? { password, confirm: password }
-        : defaultValues,
+    defaultValues: defaultValues.password === "" ? { password } : defaultValues,
   });
 
   const handleSubmit = async (data: Passwords) => {
@@ -42,43 +39,40 @@ const PasswordForm = () => {
     router.push(`/${from ?? "transactions"}`);
   };
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8 p-8">
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Lösenord</FormLabel>
-              <FormControl>
-                <Input type="password" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirm"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Repetera lösenord</FormLabel>
-              <FormControl>
-                <Input type="password" {...field} />
-              </FormControl>
-              <FormMessage />
-              <FormDescription>
-                Lösenordet används för att kryptera och avkryptera dina
-                bankloggar.
-              </FormDescription>
-            </FormItem>
-          )}
-        />
-        <Button type="submit" disabled={!form.formState.isValid}>
-          Använd lösenordet
-        </Button>
-      </form>
-    </Form>
+    <>
+      {error && (
+        <p className="p-4">
+          Fel lösenord. Kunde inte avkryptera dina bankloggar.
+        </p>
+      )}
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          className="space-y-8 p-8"
+        >
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Lösenord</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+                <FormDescription>
+                  Lösenordet används för att kryptera och avkryptera dina
+                  bankloggar.
+                </FormDescription>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={!form.formState.isValid}>
+            Använd lösenordet
+          </Button>
+        </form>
+      </Form>
+    </>
   );
 };
 
