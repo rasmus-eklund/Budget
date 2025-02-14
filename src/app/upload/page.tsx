f3e45a44b10dcf3cd66bd78cf9683aa5de060714
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-14 07:38:00 +0100

    remove hide

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -4,7 +4,6 @@ import {
   getPersonAccounts,
   getTxsPerYear,
 } from "./actions/uploadActions";
-import Hide from "./_components/Hide";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import YearCountChart from "./_components/YearCountChart";
 
@@ -18,18 +17,14 @@ const page = async () => {
   return (
     <div className="flex flex-col gap-4 p-4">
       {data.length !== 0 && (
-        <Hide>
-          <Card>
-            <CardHeader>
-              <CardTitle className="text-red-600">
-                Transaktioner per år
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <YearCountChart data={data} />
-            </CardContent>
-          </Card>
-        </Hide>
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-red-600">Transaktioner per år</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <YearCountChart data={data} />
+          </CardContent>
+        </Card>
       )}
       <FileForm categories={categories} people={personAccounts} />
     </div>
