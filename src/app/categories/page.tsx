24625c9a1d28b3142b00a5ed5b5f994de69c9ef5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-14 05:13:53 +0100

    update logic

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -17,24 +17,28 @@ const Categories = async () => {
     redirect("/");
   }
   const data = await getAllCategories();
-  data.sort((a, b) => a.name.localeCompare(b.name));
   return (
     <div className="flex flex-col gap-6 p-2">
       <h2 className="text-lg font-semibold">Dina kategorier:</h2>
       <ul className="flex flex-col gap-1">
-        {data.map(({ id, name }) => (
-          <li
-            className="border-b-red flex h-8 items-center justify-between border-b"
-            key={id}
-          >
-            <Link href={`/categories/${id}`}>{capitalize(name)}</Link>
-            <form action={removeCategory}>
-              <input hidden name="id" type="text" defaultValue={id} />
-              <DeleteButton />
-            </form>
-          </li>
-        ))}
-        {data.length === 0 ? <li>Du har inga kategorier än.</li> : null}
+        {data.length === 0 ? (
+          <li>Du har inga kategorier än.</li>
+        ) : (
+          data
+            .toSorted((a, b) => a.name.localeCompare(b.name))
+            .map(({ id, name }) => (
+              <li
+                className="border-b-red flex h-8 items-center justify-between border-b"
+                key={id}
+              >
+                <Link href={`/categories/${id}`}>{capitalize(name)}</Link>
+                <form action={removeCategory}>
+                  <input hidden name="id" type="text" defaultValue={id} />
+                  <DeleteButton />
+                </form>
+              </li>
+            ))
+        )}
       </ul>
       <AddItemForm
         onSubmit={addCategory}
