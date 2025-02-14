d7a48db322690e2de93acbc9d25af4121b13ab85
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-14 05:23:24 +0100

    change logic

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -19,38 +19,42 @@ const Categories = async () => {
     redirect("/");
   }
   const data = await getAllPeople();
-  data.sort((a, b) => a.name.localeCompare(b.name));
 
   return (
     <div className="flex flex-col gap-6 p-2">
       <h2 className="text-lg font-semibold">Dina personer:</h2>
       <ul className="flex flex-col gap-1">
-        {data.map(({ id, name }) => (
-          <li
-            className="border-b-red flex h-8 items-center justify-between border-b"
-            key={id}
-          >
-            <Link href={`/people/${name}`}>{capitalize(name)}</Link>
-            <div className="flex items-center gap-2">
-              <EditItemForm
-                data={{ name, id }}
-                onSubmit={renamePerson}
-                formInfo={{
-                  description: "Detta kommer att ändra personens namn",
-                  label: "Namn",
-                }}
-                uniques={data.map((i) => i.name)}
-              />
-              <DeleteDialog info={{ title: "personen" }}>
-                <form action={removePerson} className="flex items-center">
-                  <input hidden name="id" type="text" defaultValue={id} />
-                  <DeleteButton icon={false} />
-                </form>
-              </DeleteDialog>
-            </div>
-          </li>
-        ))}
-        {data.length === 0 ? <li>Du har inga personer än.</li> : null}
+        {!data || data.length === 0 ? (
+          <li>Du har inga personer än.</li>
+        ) : (
+          data
+            .toSorted((a, b) => a.name.localeCompare(b.name))
+            .map(({ id, name }) => (
+              <li
+                className="border-b-red flex h-8 items-center justify-between border-b"
+                key={id}
+              >
+                <Link href={`/people/${name}`}>{capitalize(name)}</Link>
+                <div className="flex items-center gap-2">
+                  <EditItemForm
+                    data={{ name, id }}
+                    onSubmit={renamePerson}
+                    formInfo={{
+                      description: "Detta kommer att ändra personens namn",
+                      label: "Namn",
+                    }}
+                    uniques={data.map((i) => i.name)}
+                  />
+                  <DeleteDialog info={{ title: "personen" }}>
+                    <form action={removePerson} className="flex items-center">
+                      <input hidden name="id" type="text" defaultValue={id} />
+                      <DeleteButton icon={false} />
+                    </form>
+                  </DeleteDialog>
+                </div>
+              </li>
+            ))
+        )}
       </ul>
       <AddItemForm
         onSubmit={addPerson}
