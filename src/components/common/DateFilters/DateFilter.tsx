89f2a1c911046874a2353ca67a6b78557c564701
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-09-07 12:12:17 +0200

    use media query to show filter

--- src/components/common/DateFilters/DateFilter.tsx
+++ src/components/common/DateFilters/DateFilter.tsx
@@ -6,11 +6,25 @@ import type { FromTo } from "~/lib/zodSchemas";
 import { TabsContent, TabsList, TabsTrigger, Tabs } from "~/components/ui/tabs";
 import FreeDay from "./Day";
 import Year from "./Year";
+import { useMediaQuery } from "~/hooks/use-media-query";
+import Drawer from "../Drawer";
 
 type Props = { changeDates: (dates: FromTo) => Promise<void>; range: FromTo };
 const DateFilter = ({ changeDates, range }: Props) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+  if (isDesktop) return <TabsDesktop changeDates={changeDates} range={range} />;
   return (
-    <Tabs defaultValue="month">
+    <div className="flex items-center px-2 pt-2 absolute right-1 top-1">
+      <Drawer icon="CalendarCog" title="Datumfilter">
+        <TabsDesktop changeDates={changeDates} range={range} />
+      </Drawer>
+    </div>
+  );
+};
+
+const TabsDesktop = ({ changeDates, range }: Props) => {
+  return (
+    <Tabs defaultValue="month" className="pt-2">
       <TabsList>
         <TabsTrigger value="month">Månad</TabsTrigger>
         <TabsTrigger value="day">Dag</TabsTrigger>
