992d4f90d3d8f66ccb0c3c5ed988879db23d6bee
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-09-07 12:11:43 +0200

    add tooltip

---
+++ src/components/common/Tooltip.tsx
@@ -0,0 +1,24 @@
+import {
+  Tooltip as TooltipShad,
+  TooltipContent,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+
+const Tooltip = ({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: string;
+}) => {
+  return (
+    <TooltipShad>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <TooltipContent>
+        <p>{title}</p>
+      </TooltipContent>
+    </TooltipShad>
+  );
+};
+
+export default Tooltip;
