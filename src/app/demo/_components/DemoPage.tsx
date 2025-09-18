3aaa6f11c89ebf2c5216e437d0cad93932aecab1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-09-18 21:15:22 +0200

    fix demo page

--- src/app/demo/_components/DemoPage.tsx
+++ src/app/demo/_components/DemoPage.tsx
@@ -1,51 +1,33 @@
 "use client";
 
 import type { FromTo } from "~/lib/zodSchemas";
-import type { Tx } from "~/types";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo } from "react";
 import { applyCategory } from "~/lib/utils/categorize";
 import ShowData from "~/components/common/ShowData";
 import DateFilter from "~/components/common/DateFilters/DateFilter";
 import { categories } from "./_generateData/categories";
 import { generateData } from "./_generateData/generateData";
-import { useTxFilterStore } from "~/stores/tx-filter-store";
+import { useStore } from "~/stores/tx-store";
 
 const DemoPage = () => {
-  const { txs, range } = useMemo(() => generateData(), []);
-  const { setTxFilter, setDefaultTxFilter } = useTxFilterStore();
-  const fromTo = { from: new Date("2023-12-01"), to: new Date("2023-12-31") };
-  const [{ from, to }, setDates] = useState<FromTo>(fromTo);
-  const data: Tx[] = useMemo(
-    () =>
-      txs
-        .filter((i) => i.datum >= from && i.datum <= to)
-        .map((i) => applyCategory({ tx: i, categories })),
-    [txs, from, to],
-  );
+  const data = useMemo(() => generateData(), []);
+  const { setTxs, setRange } = useStore();
 
-  const applyDefaultFilters = useCallback(
-    (txs: Tx[]) => {
-      const category = [...new Set(txs.map((i) => i.budgetgrupp))].filter(
-        (i) => i !== "inom",
-      );
-      const person = [...new Set(txs.map((i) => i.person))];
-      const account = [...new Set(txs.map((i) => i.konto))];
-      setTxFilter({ account, category, person, search: "" });
-      setDefaultTxFilter({ account, category, person, search: "" });
-    },
-    [setTxFilter, setDefaultTxFilter],
-  );
+  const changeDates = async ({ from, to }: FromTo) => {
+    const txs = data.txs
+      .filter((i) => i.datum >= from && i.datum <= to)
+      .map((i) => applyCategory({ tx: i, categories }));
+    setTxs(txs);
+  };
 
   useEffect(() => {
-    applyDefaultFilters(data);
-  }, [applyDefaultFilters, data]);
+    const fromTo = { from: new Date("2023-12-01"), to: new Date("2023-12-31") };
+    setRange(fromTo);
+  }, [setRange]);
 
   return (
-    <ShowData data={data}>
-      <DateFilter
-        range={range}
-        changeDates={async (dates) => setDates(dates)}
-      />
+    <ShowData>
+      <DateFilter changeDates={changeDates} />
     </ShowData>
   );
 };
