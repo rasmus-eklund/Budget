875dae1b57c6317b4abd091c4459b00d9beb43b1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-12-06 08:00:01 +0100

    refactor

--- src/components/common/LoadingItems.tsx
+++ src/components/common/LoadingItems.tsx
@@ -6,46 +6,14 @@ const LoadingItems = ({ page }: Props) => {
     <div className="flex flex-col gap-6 p-2">
       <h2 className="text-lg font-semibold">Dina {page.toLowerCase()}er:</h2>
       <div className="flex flex-col gap-1">
-        <div className="flex justify-between border-b py-1">
-          <Skeleton className="h-[20px] w-[250px] rounded-2xl" />
-          <div className="flex gap-2">
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-          </div>
-        </div>
-        <div className="flex justify-between border-b py-1">
-          <Skeleton className="h-[20px] w-[250px] rounded-2xl" />
-          <div className="flex gap-2">
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-          </div>
-        </div>
-        <div className="flex justify-between border-b py-1">
-          <Skeleton className="h-[20px] w-[250px] rounded-2xl" />
-          <div className="flex gap-2">
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-          </div>
-        </div>
-        <div className="flex justify-between border-b py-1">
-          <Skeleton className="h-[20px] w-[250px] rounded-2xl" />
-          <div className="flex gap-2">
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-          </div>
-        </div>
-        <div className="flex justify-between border-b py-1">
-          <Skeleton className="h-[20px] w-[250px] rounded-2xl" />
-          <div className="flex gap-2">
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-            <Skeleton className="h-[20px] w-[20px] rounded-full" />
-          </div>
-        </div>
+        {Array.from({ length: 6 }).map((_, i) => (
+          <Item key={i} />
+        ))}
       </div>
       <div className="space-y-8">
         <div className="space-y-2">
           <Label>{page}</Label>
-          <Skeleton className="h-[30px] w-full py-[4px]" />
+          <Skeleton className="h-[30px] w-full py-1" />
           <p className="text-sm text-muted-foreground">
             Lägg till en ny {page.toLowerCase()}.
           </p>
@@ -55,5 +23,15 @@ const LoadingItems = ({ page }: Props) => {
     </div>
   );
 };
+
+const Item = () => (
+  <div className="flex justify-between border-b py-1">
+    <Skeleton className="h-5 w-full rounded-2xl" />
+    <div className="flex gap-2">
+      <Skeleton className="size-5 rounded-full" />
+      <Skeleton className="size-5 rounded-full" />
+    </div>
+  </div>
+);
 
 export default LoadingItems;
