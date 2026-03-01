b393cdcaab8f903e682d7406d9f7cdbb85fb21ec
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-03-01 07:54:19 +0100

    only get user

--- src/server/getUserId.tsx
+++ src/server/getUserId.tsx
@@ -2,15 +2,9 @@ import { redirect } from "next/navigation";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const getUserId = async () => {
-  const { isAuthenticated, getUser } = getKindeServerSession();
-  const isUserAuthenticated = await isAuthenticated();
-  if (!isUserAuthenticated) {
-    redirect("/api/auth/login");
-  }
+  const { getUser } = getKindeServerSession();
   const user = await getUser();
-  if (!user) {
-    redirect("/api/auth/login");
-  }
+  if (!user) redirect("/api/auth/login");
   return user.id;
 };
 
