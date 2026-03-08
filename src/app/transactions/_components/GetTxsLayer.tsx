97c04eb4c39fd58c29f06c651d0234b349e260e4
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-03-08 13:36:16 +0100

    add initial loading spinner

--- src/app/transactions/_components/GetTxsLayer.tsx
+++ src/app/transactions/_components/GetTxsLayer.tsx
@@ -1,14 +1,16 @@
 "use client";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { type FromTo } from "~/lib/zodSchemas";
-import { getLastMonthYear } from "~/lib";
+import { getErrorMessage, getLastMonthYear } from "~/lib";
 import getTxByDates from "../dataLayer/getData";
 import { useStore } from "~/stores/tx-store";
 import { emptyOptions } from "~/constants";
-import { ShowData } from "~/components/common";
+import { ShowData, Spinner } from "~/components/common";
+import { toast } from "sonner";
 
 type Props = { range: FromTo; userId: string };
 const GetTxsLayer = ({ range: { from, to }, userId }: Props) => {
+  const [isReady, setIsReady] = useState(false);
   const setTxs = useStore((state) => state.setTxs);
   const setLoading = useStore((state) => state.setLoading);
   const setRange = useStore((state) => state.setRange);
@@ -25,7 +27,7 @@ const GetTxsLayer = ({ range: { from, to }, userId }: Props) => {
         setSelectedRange(dates);
         setTxs({ txs: res.data, options: res.options, reset });
       } catch (e) {
-        console.error(e);
+        toast.error(getErrorMessage(e));
         setTxs({ txs: [], options: emptyOptions, reset: true });
       } finally {
         setLoading(false);
@@ -38,11 +40,12 @@ const GetTxsLayer = ({ range: { from, to }, userId }: Props) => {
     const range = { from, to };
     setRange(range);
     const dates = getLastMonthYear(range);
-    getData(dates, true);
+    getData(dates, true)
+      .catch((e) => toast.error(getErrorMessage(e)))
+      .finally(() => setIsReady(true));
   }, [setRange, from, to, getData]);
-
+  if (!isReady) return <Spinner />;
   return <ShowData changeDates={getData} canMarkInternal />;
 };
 
 export default GetTxsLayer;
-
