fec1ebde37b91f856290b8aa266253e48e14b5b3
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-05-02 22:44:44 +0200

    use compiler

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const config: NextConfig = {
   output: "standalone",
   crossOrigin: "anonymous",
+  reactCompiler: true,
   allowedDevOrigins: ["localhost", "127.0.0.1"],
   images: { remotePatterns: [{ hostname: "lh3.googleusercontent.com" }] },
 };
