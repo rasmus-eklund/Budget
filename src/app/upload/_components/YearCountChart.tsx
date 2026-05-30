929e79fba56b980d116cff6e8ed8dd8ab28fa804
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-05-30 17:32:53 +0200

    make collapsible

--- src/app/upload/_components/YearCountChart.tsx
+++ src/app/upload/_components/YearCountChart.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from "react";
 import {
   Bar,
   BarChart,
@@ -8,20 +9,45 @@ import {
   XAxis,
   YAxis,
 } from "recharts";
+import { Icon } from "~/components/common";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui";
 
 type Props = { data: { year: number; count: number }[] };
 
 const YearCountChart = ({ data }: Props) => {
+  const [open, setOpen] = useState(true);
   return (
-    <ResponsiveContainer height={200} minWidth={"100%"}>
-      <BarChart data={data}>
-        <CartesianGrid />
-        <Tooltip cursor={false} />
-        <XAxis dataKey={"year"} />
-        <YAxis dataKey={"count"} />
-        <Bar dataKey={"count"} name={"Antal"} fill="#dc2626" />
-      </BarChart>
-    </ResponsiveContainer>
+    <Card className="py-0">
+      <CardHeader className="gap-0 p-0">
+        <Button
+          onClick={() => setOpen((p) => !p)}
+          variant="ghost"
+          className="flex items-center justify-between gap-2"
+        >
+          <CardTitle className="text-primary">Transaktioner per år</CardTitle>
+          <Icon icon={open ? "ChevronUp" : "ChevronDown"} />
+        </Button>
+      </CardHeader>
+      {open && (
+        <CardContent>
+          <ResponsiveContainer height={200} minWidth={"100%"}>
+            <BarChart data={data}>
+              <CartesianGrid />
+              <Tooltip cursor={false} />
+              <XAxis dataKey={"year"} />
+              <YAxis dataKey={"count"} />
+              <Bar dataKey={"count"} name={"Antal"} fill="#dc2626" />
+            </BarChart>
+          </ResponsiveContainer>
+        </CardContent>
+      )}
+    </Card>
   );
 };
 
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -4,7 +4,6 @@ import {
   getPersonAccounts,
   getTxsPerYear,
 } from "./actions/uploadActions";
-import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui";
 import YearCountChart from "./_components/YearCountChart";
 import WithAuth from "~/components/server/WithAuth";
 
@@ -16,17 +15,9 @@ const UploadPage = async () => {
   ]);
 
   return (
-    <div className="flex size-full flex-col gap-4 p-4">
-      {data.length !== 0 && (
-        <Card>
-          <CardHeader>
-            <CardTitle className="text-primary">Transaktioner per år</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <YearCountChart data={data} />
-          </CardContent>
-        </Card>
-      )}
+    <div className="flex size-full flex-col gap-4 overflow-auto p-4">
+      {data.length !== 0 && <YearCountChart data={data} />}
+
       <FileForm categories={categories} people={personAccounts} />
     </div>
   );
